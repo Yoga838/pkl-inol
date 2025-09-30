5c8d68a9de07a5c1b1f6b04b0f507287d53d9c76
Yoga838 <212410102031@mail.unej.ac.id>
2025-09-30 10:35:11 +0700

    session login in 7 day

--- src/Components/Fragments/protectedRoute.jsx
+++ src/Components/Fragments/protectedRoute.jsx
@@ -1,14 +1,32 @@
+import React, { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("auth_fullname");
+  const authEmail = localStorage.getItem("auth_email");
+  const loginTime = localStorage.getItem("tipe_time");
 
-  // Kalau belum login → redirect ke /login
-  if (!isLoggedIn) {
-    alert("⚠️ Anda harus login dulu untuk mengakses halaman ini!");
-    return<Navigate to="/" replace />;
+  useEffect(() => {
+    if (loginTime) {
+      const loginDate = new Date(loginTime);
+      const now = new Date();
+
+      // Hitung selisih waktu dalam ms
+      const diff = now - loginDate;
+      const sevenDays = 7 * 24 * 60 * 60 * 1000; // 7 hari
+
+      if (diff >= sevenDays) {
+        // Expired → hapus data login
+        localStorage.clear();
+        alert("Sesi login Anda telah kedaluwarsa. Silakan login kembali.");
+        window.location.href = "/"; // redirect manual
+      }
+    }
+  }, [loginTime]);
+
+  if (!authEmail) {
+    alert("Anda harus login terlebih dahulu!");
+    return <Navigate to="/" replace />;
   }
 
-  // Kalau login → render halaman aslinya
   return children;
 }
